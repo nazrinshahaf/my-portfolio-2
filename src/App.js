eb0678af70325c00910afe4d1d247e7b9be20500
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2020-05-19 17:24:22 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Home from "../src/Home";
+import Navbar from "../src/Navbar";
+import Projects from "../src/Projects";
+
+import "./App.scss";
 
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScrollY = () => {
+      const position = document.body.scrollTop;
+      setScrollPosition(position);
+    };
+
+    document.body.addEventListener("scroll", handleScrollY, { passive: true });
+    return () => window.removeEventListener("scroll", handleScrollY);
+  }, scrollPosition);
+
+  // console.log("Scroll position = ", scrollPosition);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar scrollPosition={scrollPosition}></Navbar>
+      <Home></Home>
+      <Projects></Projects>
     </div>
   );
 }
